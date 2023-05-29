67803641741d6f4e5e30f3fbdd34b56b251dfb2c
Kevin Albertson <kevin.eric.albertson@gmail.com>
2023-05-29 14:02:19 -0400

    add leaderboard

---
+++ leaderboard/submit.php
@@ -0,0 +1,121 @@
+<?php
+
+require_once("secrets.php");
+
+// Create/open the database
+$db = new SQLite3('tetris.db');
+
+function errexit ($msg) {
+    global $db;
+    echo json_encode ([
+        "ok" => 0,
+        "msg" => $msg
+    ]);
+    $db->close();
+    exit();
+}
+
+if (!isset($_POST["token"])) {
+    errexit("Expected 'token' but got none");
+}
+
+// Verify token.
+$url = 'https://www.google.com/recaptcha/api/siteverify';
+$data = array('secret' => $SECRET_KEY, 'response' => $_POST["token"], 'remoteip' => $_SERVER["REMOTE_ADDR"]);
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context = stream_context_create($options);
+$result_str = file_get_contents($url, false, $context);
+if ($result_str === false) {
+    errexit ("Failed to send request to verify token");
+}
+
+$result = json_decode($result_str, $associative=true);
+if (!$result) {
+    errexit ("reCaptcha response failed to JSON decode: " . $result_str);
+}
+
+if (!isset($result["success"])) {
+    errexit ("reCaptcha token did not have 'success' field.");
+}
+
+if (!$result["success"]) {
+    errexit ("reCaptcha token did not succeed. Try refreshing the page and submitting again.");
+}
+
+// From https://developers.google.com/recaptcha/docs/v3#interpreting_the_score:
+// "By default, you can use a threshold of 0.5."
+if ($result["score"] < .5) {
+    errexit ("reCaptcha token did not have high enough score. Try refreshing the page and submitting again.");
+}
+
+if (!isset($_POST["name"])) {
+    errexit("Expected 'name' but got none");
+}
+$name = $_POST["name"];
+if (strlen($name) > 127) {
+    errexit("Expected 'name' to be <= 127 characters, got: " . strlen($name));
+}
+
+function get_int_field ($key) {
+    if (!isset($_POST[$key])) {
+        errexit("Expected '" . $key . "' but got none");
+    }
+    if (!is_numeric ($_POST[$key])) {
+        errexit("Expected int '" . $key . "' but got:" . $_POST[$key]);
+    }
+    return intval($_POST[$key]);
+}
+
+$score = get_int_field("score");
+$level = get_int_field("level");
+$lines = get_int_field("lines");
+$date = get_int_field("date");
+
+// Create a table
+$query = "CREATE TABLE IF NOT EXISTS highscores (
+    name TEXT,
+    score INTEGER,
+    level INTEGER,
+    lines INTEGER,
+    date INTEGER
+)";
+$db->exec($query);
+
+// Check if database already has entry. 
+$stmt = $db->prepare("SELECT * FROM highscores WHERE name=:name AND score=:score AND level=:level AND lines=:lines AND date=:date");
+$stmt->bindValue(":name", $name, SQLITE3_TEXT);
+$stmt->bindValue(":score", $score, SQLITE3_INTEGER);
+$stmt->bindValue(":level", $level, SQLITE3_INTEGER);
+$stmt->bindValue(":lines", $lines, SQLITE3_INTEGER);
+$stmt->bindValue(":date", $date, SQLITE3_INTEGER);
+$res = $stmt->execute();
+if ($row = $res->fetchArray()) {
+    errexit("Score already saved");
+}
+
+// Insert data into the table
+$stmt = $db->prepare("INSERT INTO highscores (name, score, level, lines, date) VALUES (:name, :score, :level, :lines, :date)");
+$stmt->bindValue(":name", $name, SQLITE3_TEXT);
+$stmt->bindValue(":score", $score, SQLITE3_INTEGER);
+$stmt->bindValue(":level", $level, SQLITE3_INTEGER);
+$stmt->bindValue(":lines", $lines, SQLITE3_INTEGER);
+$stmt->bindValue(":date", $date, SQLITE3_INTEGER);
+$res = $stmt->execute();
+
+if (!$res) {
+    errexit ("Failed to insert into database");
+}
+
+echo json_encode(["ok" => 1]);
+
+// Close the database connection
+$db->close();
+?>
---
+++ leaderboard/view.php
@@ -0,0 +1,104 @@
+<html>
+
+<head>
+    <title>Tetris</title>
+    <meta name="viewport" content="width=462, maximum-scale=1, user-scalable=no">
+    <link rel="icon" type="image/png" href="../img/favicon.png">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-HDRY5R79EL"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-HDRY5R79EL');
+    </script>
+    <style type="text/css">
+        body.dark {
+            background: #333;
+        }
+
+        body.dark h1 {
+            color: #eee;
+        }
+
+        body.dark td, body.dark th {
+            color: #eee;
+        }
+
+        th {
+            text-align: left;
+        }
+
+        body.dark a {
+            color: #FFF;
+        }
+
+        body.dark .popup a {
+            color: initial;
+        }
+
+        * {
+            padding: 0px;
+            margin: 0px;
+            font-family: "Courier New"
+        }
+
+        h1 {
+            font-size: 20px;
+        }
+
+        #main {
+            width: 454px;
+            margin: 4px auto;
+        }
+
+        #main table {
+            width: 100%;
+            margin-bottom: 10px;
+        }
+
+    </style>
+</head>
+
+<body>
+    <div id="main">
+
+    <h1>Tetris Leaderboard</h1>
+    <table>
+<?php
+
+// Create/open the database
+$db = new SQLite3('tetris.db');
+
+// Check if database already has entry. 
+$stmt = $db->prepare("SELECT * FROM highscores ORDER BY score DESC");
+$res = $stmt->execute();
+while ($row = $res->fetchArray()) {
+    echo "<table>";
+    echo "<tr><td>Name</td><td>{$row["name"]}</td></tr>";
+    echo "<tr><td>Score</td><td>{$row["score"]}</td></tr>";
+    echo "<tr><td>Level</td><td>{$row["level"]}</td></tr>";
+    echo "<tr><td>Lines</td><td>{$row["lines"]}</td></tr>";
+    echo "<tr><td>Date</td><td class='date' data-dateunix='{$row["date"]}'></td></tr>";
+    echo "</table>";
+}
+
+// Close the database connection
+$db->close();
+?>
+    </table>
+    </div>
+
+    <script>
+        if (window.localStorage.getItem("dark_mode") == "on") {
+            document.body.classList.add("dark");
+        }
+        document.querySelectorAll(".date").forEach((date_el) => {
+            const date_unix = parseInt(date_el.getAttribute("data-dateunix"));
+            const date_date = new Date(date_unix);
+            date_el.innerHTML = date_date.toLocaleString();
+        });
+    </script>
+</body>
+</html>
